f3520af2eacbc609746f78a66e837c4a3c8d3a07
Germaine Pannell <me@germainep.com>
2019-10-30 20:35:19 -0400

    created routes for Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path exact="/" />
+    <Route path exact="/about" />
+    <Route path exact="/contact" />
   </div>
 );
 
