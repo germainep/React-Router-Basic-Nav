f0fac96ddc713043648e7cd647a5e71646ddc920
Germaine Pannell <me@germainep.com>
2019-10-30 20:34:51 -0400

    impelmented react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
